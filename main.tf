a641bdf3cb6dfa759926ad5594be320c65a3a85a
Arnau Beltrà <arnau.beltra@gft.com>
2023-05-29 12:26:22 +0200

    Fixed names

--- main.tf
+++ main.tf
@@ -2,30 +2,30 @@
 module "ecommerce_network" {
   source = "./modules/networking"
 
-  network_name = "ecommerce_network"
+  network_name = "ecommerce-network"
   subnetworks = [ 
     {
-        subnetwork_name = "webapp_prod_1"
+        subnetwork_name = "webapp-prod-1"
         ip_cidr_range = "10.0.1.0/24"
         region = "europe-west1-b"
     },
     {
-        subnetwork_name = "webapp_prod_2"
+        subnetwork_name = "webapp-prod-2"
         ip_cidr_range = "10.0.2.0/24"
         region = "europe-west1-c"
     },
     {
-        subnetwork_name = "webapp_prod_3"
+        subnetwork_name = "webapp-prod-3"
         ip_cidr_range = "10.0.3.0/24"
         region = "europe-west1-d"
     },
     {
-        subnetwork_name = "database_prod"
+        subnetwork_name = "database-prod"
         ip_cidr_range = "10.0.4.0/24"
         region = "europe-west1-b"
     },
     {
-        subnetwork_name = "database_standby_prod"
+        subnetwork_name = "database-standby-prod"
         ip_cidr_range = "10.0.5.0/24"
         region = "europe-west1-c"
     },
@@ -44,13 +44,13 @@ module "ecommerce_network" {
 module "web_servers_instance_group" {
   source = "./modules/instances"
 
-  instance_template_name = "web_servers_ins_tem"
+  instance_template_name = "web-servers-ins-tem"
   machine_type = "e2-medium"
   source_image = "debian-cloud/debian-11"
-  network = "ecommerce_network"
-  subnetwork = "webapp_prod_1"
+  network = "ecommerce-network"
+  subnetwork = "webapp-prod-1"
 
-  health_check_name = "web_servers_health_check"
+  health_check_name = "web-servers-health-check"
   check_interval_sec = 5
   timeout_sec = 2
   healthy_threshold = 2
@@ -58,15 +58,15 @@ module "web_servers_instance_group" {
   request_path = "/"
   port = "80"
 
-  autoscaler_name = "web_server_autoscaler"
+  autoscaler_name = "web-server-autoscaler"
   autoscaler_zone = "europe-west1-b"
   max_replicas = 5
   min_replicas = 1
   cooldown_period = 300
   target_cpu_utilization = 0.6
 
-  instance_group_name = "web_servers_ins_group"
-  base_instance_name = "web_server"
+  instance_group_name = "web-servers-ins-group"
+  base_instance_name = "web-server"
   main_instance_group_zone = "europe-west1-b"
   distribution_policy_zones = ["europe-west1-b", "europe-west1-c", "europe-west1-d"]
   initial_delay_sec = 300
