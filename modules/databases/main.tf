9751fc68f8e76b4d0fcad2b12f6745ebb86ac7f0
Arnau Beltrà <arnau.beltra@gft.com>
2023-05-30 10:57:31 +0200

    Changed databse IP type

--- modules/databases/main.tf
+++ modules/databases/main.tf
@@ -1,4 +1,8 @@
-resource "google_compute_global_address" "private_ip_address" {
+# Database was suposed to only have private IP address, but due to organizational permissions
+# it will have a public IP address, as I cannot set a static IP address. All code disabled
+# below it will be for this reason.
+
+/*resource "google_compute_global_address" "private_ip_address" {
   name          = var.private_ip_address_name
   address_type  = "INTERNAL"
   prefix_length = var.prefix_length
@@ -10,13 +14,14 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
+*/
 
 resource "google_sql_database_instance" "database_master" {
   name                = var.database_name
   region              = var.database_region
   database_version    = var.database_version
   deletion_protection = false
-  depends_on          = [google_service_networking_connection.private_vpc_connection]
+  #depends_on          = [google_service_networking_connection.private_vpc_connection]
 
   settings {
     tier              = var.database_tier
@@ -28,9 +33,10 @@ resource "google_sql_database_instance" "database_master" {
     }
 
     ip_configuration {
-      ipv4_enabled                                  = false
-      private_network                               = var.network
-      enable_private_path_for_google_cloud_services = true
+      ipv4_enabled = true
+      #ipv4_enabled                                  = false
+      #private_network                               = var.network
+      #enable_private_path_for_google_cloud_services = true
     }
   }
 }
@@ -41,7 +47,7 @@ resource "google_sql_database_instance" "read_replica" {
   region               = var.read_replica_region
   database_version     = var.database_version
   deletion_protection  = false
-  depends_on           = [google_service_networking_connection.private_vpc_connection]
+  #depends_on           = [google_service_networking_connection.private_vpc_connection]
 
   replica_configuration {
     failover_target = false
@@ -57,9 +63,10 @@ resource "google_sql_database_instance" "read_replica" {
     }
 
     ip_configuration {
-      ipv4_enabled                                  = false
-      private_network                               = var.network
-      enable_private_path_for_google_cloud_services = true
+      ipv4_enabled = true
+      #ipv4_enabled                                  = false
+      #private_network                               = var.network
+      #enable_private_path_for_google_cloud_services = true
     }
   }
 }
