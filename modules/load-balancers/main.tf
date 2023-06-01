0cf62a418b2bd2d2e339ea6847ba8b630360d138
Arnau Beltrà <arnau.beltra@gft.com>
2023-06-01 12:27:48 +0200

    Fix internal lb

--- modules/load-balancers/main.tf
+++ modules/load-balancers/main.tf
@@ -14,7 +14,6 @@ resource "google_compute_region_backend_service" "lb_backend" {
   load_balancing_scheme = var.load_balancing_scheme
   health_checks         = [google_compute_region_health_check.lb_health_check.id]
   protocol = var.protocol
-  network = var.network
   backend {
     group           = var.group
     balancing_mode = var.balancing_mode
